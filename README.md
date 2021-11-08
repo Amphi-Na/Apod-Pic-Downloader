513e3588592772252b2ed37b827c469d8f1b4745
Atrophied Prokaryote <rutwiks123@gmail.com>
2021-11-09 00:57:09 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Apod-Pic-Downloader
 This is a python script through which you can download the Apod-Astronomy Picture Of the Day. 
+
+- First thing you need to do is go to [Nasa API Website](https://api.nasa.gov) and get your api key.
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+
+para = {'api_key': #your api key from https://api.nasa.gov inside double quotes}
+json = (requests.get('https://api.nasa.gov/planetary/apod', params=para)).json()
+file = open(json["date"] + " - " + json["title"] + "." + (json["url"].split("."))[-1], "wb")
+file.write((requests.get(json["hdurl"])).content)
+file.close()
